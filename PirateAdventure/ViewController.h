33a11fa3f0848612a86024663267684369462e8b
Avinash Prabhakar <avinashsp93@gmail.com>
2017-09-13 19:05:44 +0530

    Initial Commit

---
+++ PirateAdventure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PirateAdventure
+//
+//  Created by Avinash P08 on 13/09/17.
+//  Copyright © 2017 Infosys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PirateAdventure/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PirateAdventure
+//
+//  Created by Avinash P08 on 13/09/17.
+//  Copyright © 2017 Infosys. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
